212f309a527a0a6441470ad1a134db90deb5243e
omrfrkdyr <omrfrkdyr@gmail.com>
2026-06-29 11:12:38 +0300

    Scheduled added, JUnit tested

--- src/main/java/com/omerduyar/scheduled/ScheduledExample.java
+++ src/main/java/com/omerduyar/scheduled/ScheduledExample.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class ScheduledExample {
 
     // * * * * * *
-    @Scheduled(cron = "*/5 * * * * *") //her 5 saniyede bir çalıştır
+    //@Scheduled(cron = "*/5 * * * * *") //her 5 saniyede bir çalıştır
     public void write1to10(){
 
         for(int i= 1; i<=10; i++){
--- src/test/java/com/omerduyar/ExceptionManagementApplicationTests.java
+++ src/test/java/com/omerduyar/ExceptionManagementApplicationTests.java
@@ -1,13 +1,28 @@
 package com.omerduyar;
 
+import com.omerduyar.dto.DtoEmployee;
+import com.omerduyar.service.IEmployeeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = {ExceptionManagementApplication.class})
 class ExceptionManagementApplicationTests {
 
+    @Autowired
+    private IEmployeeService employeeService;
+
+    
+
+
     @Test
-    void contextLoads() {
+    public void testFindEmployeeById(){
+        DtoEmployee employee = employeeService.findEmployeeById(4L);
+
+        assertNotNull(5);
+
     }
 
 }
